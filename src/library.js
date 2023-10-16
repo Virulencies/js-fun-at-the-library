312ac65a3747a5900aef9a890966a2cf265273af
Theo McCray <tlmccraycray@gmail.com>
2023-10-15 23:42:21 -0600

    Initial commit

--- src/library.js
+++ src/library.js
@@ -1,8 +1,32 @@
 
+function createLibrary(libName) {
+  return {
+    name: libName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+
+    }
+
+  };
+}
+
+function addBook(library, book) {
+  if (book.genre === fantasy) {
+    library.shelves.fantasy.push(book);
+  }
+  else if (book.genre === fiction) {
+    library.shelves.fiction.push(book);
+  }
+  else if (book.genre === nonFiction) {
+    library.shelves.nonFiction.push(book);
+  }
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+   createLibrary,
+   addBook,
   // checkoutBook,
   // takeStock
 };
