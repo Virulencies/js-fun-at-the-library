a37e90102d5bcc82e823fd977513431079b7a350
Theo McCray <tlmccraycray@gmail.com>
2023-10-15 23:45:01 -0600

    Change genres values to strings

--- src/library.js
+++ src/library.js
@@ -13,13 +13,13 @@ function createLibrary(libName) {
 }
 
 function addBook(library, book) {
-  if (book.genre === fantasy) {
+  if (book.genre === "fantasy") {
     library.shelves.fantasy.push(book);
   }
-  else if (book.genre === fiction) {
+  else if (book.genre === "fiction") {
     library.shelves.fiction.push(book);
   }
-  else if (book.genre === nonFiction) {
+  else if (book.genre === "nonFiction") {
     library.shelves.nonFiction.push(book);
   }
 }
