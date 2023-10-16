1080797399e4b7e6750981dd68f8529fa14ad10e
Theo McCray <tlmccraycray@gmail.com>
2023-10-15 21:38:43 -0600

    Initial commit

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,12 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
